35a43d2274bfac35479381260c42deeb7de4523b
prasantib <prasanti.babu@gmail.com>
2019-10-25 14:52:41 -0400

    finished arrays

--- src/Main.java
+++ src/Main.java
@@ -46,14 +46,65 @@ public class Main {
 }
 
  */
+/*import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String [] names = new String[4];
-        for (int i = 0; i < 4; i++) {
+        System.out.println("Please enter 3 names: ");
+        String [] names = new String[3];
+        for (int i = 0; i < 3; i++) {
             names[i] = scan.nextLine();
         }
-        System.out.println(names);
+        System.out.println(Arrays.toString(names));
+    }
+}
+
+ */
+
+import java.util.Arrays;
+
+/*public class Main {
+    public static void main(String[] args) {
+        int [] numbers = new int[]{12,5,9,0,2,1,8,5,4};
+        for (int i = 5; i < 8; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
+ */
+/*public class Main {
+    public static void main(String[] args) {
+        int [] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = numbers.length-1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
+
+ */
+/*public class Main {
+    public static void main(String[] args) {
+        String [] letters = new String[]{"w","t","y","h","k"};
+        for (char i = 0; i < 5; i++) {
+            if (letters[i] == "t"){
+                letters[i] = "hello";
+            }
+            }
+        System.out.println(Arrays.toString(letters));
+        }
+    }
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        int [] numbers = new int[]{1,7,6,5,9};
+        int [] numbers2 = new int[]{2,7,6,3,4};
+        for (int i = 0; i < 4; i++) {
+            if (numbers[i] == numbers2[i]){
+                System.out.println(numbers[i]);
+            }
+        }
     }
 }
